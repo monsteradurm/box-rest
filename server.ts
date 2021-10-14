86243c1101ba05f14a46d84acfc2ccf591733dd4
Adam Cranch <acranch@liquidanimation.com>
2021-10-15 09:56:49 +1000

    parsing

--- server.ts
+++ server.ts
@@ -1,33 +1,22 @@
 import {BoxHelper} from './box';
 import express, { Request, Response, NextFunction } from 'express';
 
-const httpProxy = require('http-proxy');
-const proxy = httpProxy.createProxyServer({});
-
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const router = express.Router();
 
-proxy.on('error', (e) => {
-    console.log(e);
-});
-
-
-app.options('*', cors());
 
 router.get('/', (req, res) => {
     res.json({ message: 'hooray! welcome to our api!' });
 });
-app.configure(() => {
-    app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        next();
-    });
-    app.use( express.bodyParser() );
-    app.use( express.methodOverride() );
-    app.use('/api', router);
-    app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+
+app.use('/', router);
 
 /*
 app.get('/sharefolder', (req, res) => {
@@ -39,8 +28,8 @@ app.get('/sharefolder', (req, res) => {
 });
 */
 
-app.listen(process.env.PORT, () => {
-        console.log('box-rest --> listening at:', process.env.PORT);
+app.listen(PORT, () => {
+        console.log('box-rest --> listening at:', PORT);
 });
 
 export {};
